52830a73d53ab53f4e12b324c32a959d988a9ee0
Dmytro Hirich <diman12345677@gmail.com>
2023-02-22 21:45:59 +0200

    integrating testing / db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,11 +7,12 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+const cookieSession = require('cookie-session')
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'sqlite',
-    database: 'db.sqlite',
+    database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite',
     entities: [User, Report],
     synchronize: true
   }),
@@ -18,6 +20,22 @@ import { Report } from './reports/report.entity';
     ReportsModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true
+      })
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  // this function is going be call automatically whenever
+  // app starts to listening incoming traffic
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieSession({
+      keys: ['qwewqewq']
+    })).forRoutes('*')
+  }
+}
