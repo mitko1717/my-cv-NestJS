ab162eb0ebff5711a51c45847bf78c0945a1795f
Dmytro Hirich <diman12345677@gmail.com>
2023-02-23 16:21:33 +0200

    transform string query

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,69 +1,38 @@
-// import { Controller, Patch, Param, Post, Body, UseGuards } from '@nestjs/common';
-// import { CreateReportDto } from './dtos/create-report.dto';
-// import { ReportsService } from './reports.service';
-// import { AuthGuard } from '../guards/auth.guard';
-// import { CurrentUser } from "src/users/decorators/current-user.decorators";
-// import { User } from 'src/users/user.entity';
-// import { ReportDto } from './dtos/report.dto';
-// import { Serialize } from 'src/interceptors/serealize.interceptor';
-// import { ApproveReportDto } from './dtos/approve-report.dto';
-// import { AdminGuard } from '../guards/admin.guard';
+import { Controller, Patch, Param, Post, Body, UseGuards, Get, Query } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from "src/users/decorators/current-user.decorators";
+import { User } from 'src/users/user.entity';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from 'src/interceptors/serealize.interceptor';
+import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
-// @Controller('reports')
-// export class ReportsController {
-//     constructor(private reportsService: ReportsService) {}
-
-//     @Post()
-//     // @UseGuards ensures that user is signed in before he can make a report
-//     @UseGuards(AuthGuard)
-//     // Serialize stands for i wanna serialize outgoing response following the
-//     // rules we set up in ReportDto
-//     @Serialize(ReportDto)
-//     createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {        
-//         return this.reportsService.create(body, user);
-//     }
-
-//     @Patch('/:id')
-//     @UseGuards(AdminGuard)
-//     // only admin use be able to approve a report
-//     async approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
-//         return this.reportsService.changeApproval(id, body.approved)
-//     }
-// }
-
-import {
-    Controller,
-    Post,
-    Body,
-    UseGuards,
-    Patch,
-    Param,
-  } from '@nestjs/common';
-  import { CreateReportDto } from './dtos/create-report.dto';
-  import { ReportsService } from './reports.service';
-  import { AuthGuard } from '../guards/auth.guard';
-  import { CurrentUser } from 'src/users/decorators/current-user.decorators';
-  import { User } from '../users/user.entity';
-  import { ReportDto } from './dtos/report.dto';
-  import { Serialize } from 'src/interceptors/serealize.interceptor';
-  import { ApproveReportDto } from './dtos/approve-report.dto';
-  import { AdminGuard } from '../guards/admin.guard';
-  
-  @Controller('reports')
-  export class ReportsController {
+@Controller('reports')
+export class ReportsController {
     constructor(private reportsService: ReportsService) {}
-  
-    @Post()
-    @UseGuards(AuthGuard)
-    @Serialize(ReportDto)
-    createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
-      return this.reportsService.create(body, user);
+
+    @Get()
+    getEstimate(@Query() query: GetEstimateDto) {
+        
     }
-  
+
+    @Post()
+    // @UseGuards ensures that user is signed in before he can make a report
+    @UseGuards(AuthGuard)
+    // Serialize stands for i wanna serialize outgoing response following the
+    // rules we set up in ReportDto
+    @Serialize(ReportDto)
+    createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {        
+        return this.reportsService.create(body, user);
+    }
+
     @Patch('/:id')
     @UseGuards(AdminGuard)
-    approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
-      return this.reportsService.changeApproval(id, body.approved);
+    // only admin use be able to approve a report
+    async approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+        return this.reportsService.changeApproval(id, body.approved)
     }
-  }
-  
+}
