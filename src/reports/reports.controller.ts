377063f0104af5bd8d4e3bfeee8bf0e3a78fbabf
Dmytro Hirich <diman12345677@gmail.com>
2023-02-23 13:15:54 +0200

    add changeApproval

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Patch, Param, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
@@ -6,6 +6,7 @@ import { CurrentUser } from "src/users/decorators/current-user.decorators";
 import { User } from 'src/users/user.entity';
 import { ReportDto } from './dtos/report.dto';
 import { Serialize } from 'src/interceptors/serealize.interceptor';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -19,5 +20,10 @@ export class ReportsController {
     @Serialize(ReportDto)
     createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {        
         return this.reportsService.create(body, user);
+    }
+
+    @Patch('/:id')
+    async approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+        return this.reportsService.changeApproval(id, body.approved)
     }
 }
