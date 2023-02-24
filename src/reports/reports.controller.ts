3692e9dd16754c080eb3064f4fcce21e9d108177
Dmytro Hirich <diman12345677@gmail.com>
2023-02-24 15:57:16 +0200

    create query builders

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -16,7 +16,7 @@ export class ReportsController {
 
     @Get()
     getEstimate(@Query() query: GetEstimateDto) {
-        
+        return this.reportsService.createEstimate(query);
     }
 
     @Post()
